bc35cf48486e77c9e45287a503733cd4ec7bbde8
Muzjili <1679139025@qq.com>
2026-01-25 16:17:23 +0800

    change(param): 修改gimbal_yaw -> base_yaw

--- pb2025_nav_bringup/launch/rm_navigation_reality_launch.py
+++ pb2025_nav_bringup/launch/rm_navigation_reality_launch.py
@@ -230,7 +230,7 @@ def generate_launch_description():
     ld.add_action(start_robot_state_publisher_cmd)
     ld.add_action(start_livox_ros_driver2_node)
     ld.add_action(bringup_cmd)
-    ld.add_action(joy_teleop_cmd)
+    # ld.add_action(joy_teleop_cmd)
     ld.add_action(rviz_cmd)
     
 
